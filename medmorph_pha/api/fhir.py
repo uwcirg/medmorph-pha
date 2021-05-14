4893bbece5d5f10a1ece3cb71160948b9f4f8ece
Ivan Cvitkovic <ivanc@uw.edu>
2021-05-14 16:04:51 -0700

    Add view to proxy all requests to HAPI

---
+++ medmorph_pha/api/fhir.py
@@ -0,0 +1,36 @@
+import requests
+
+from flask import Blueprint, abort, current_app, request, session, g
+
+blueprint = Blueprint('fhir', __name__, url_prefix='/fhir')
+
+
+@blueprint.route('/$process-message')
+def process_message():
+    return {}
+
+
+@blueprint.route('/', defaults={'relative_path': ''})
+@blueprint.route('/<path:relative_path>')
+def route_fhir(relative_path):
+    backing_fhir_base_url = current_app.config['BACKING_FHIR_URL']
+    backing_fhir_url = '/'.join((backing_fhir_base_url, relative_path))
+    backing_headers = {}
+    if 'Authorization' in request.headers:
+        backing_headers = {'Authorization': request.headers['Authorization']}
+
+    # TODO pass all HTTP verbs, not just GET
+    backing_response = requests.get(
+        url=backing_fhir_url,
+        headers=backing_headers,
+        params=request.args,
+    )
+    return backing_response.json(), backing_response.status_code
+
+
+@blueprint.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization'
+
+    return response
