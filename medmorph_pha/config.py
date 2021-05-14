d55b05b3cf7ace1fb213b1ad50f87fdc166ceca5
Ivan Cvitkovic <ivanc@uw.edu>
2021-05-14 16:05:08 -0700

    Add HAPI fhir service, config

--- medmorph_pha/config.py
+++ medmorph_pha/config.py
@@ -8,3 +8,5 @@ SERVER_NAME = os.getenv("SERVER_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY")
 # URL scheme to use outside of request context
 PREFERRED_URL_SCHEME = os.getenv("PREFERRED_URL_SCHEME", "http")
+
+BACKING_FHIR_URL = os.getenv("BACKING_FHIR_URL ", "http://hapi:8080/hapi-fhir-jpaserver/fhir")
