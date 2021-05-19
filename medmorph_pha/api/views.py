2f27df3d4c437efbbc0bdca6bc4eed9cee5e2e90
Paul Bugni <pbugni@u.washington.edu>
2021-05-19 09:13:54 -0700

    extend table with patient details.

--- medmorph_pha/api/views.py
+++ medmorph_pha/api/views.py
@@ -24,15 +24,37 @@ def reports():
         """Obtain bundles and metadata such as lastUpdated for each"""
         results = []
         all_bundles = route_fhir(relative_path='Bundle').json
-        for bundle in all_bundles['entry']:
+        for bundle in all_bundles.get('entry',[]):
             # Don't know join syntax, loop over and request metadata
             id = bundle["resource"]["id"]
             relative_path = f'Bundle/{id}'
             bundle_resource = route_fhir(relative_path=relative_path).json
-            results.append({
+            row = {
                 'id': id,
                 'url': url_for('fhir.route_fhir', relative_path=relative_path),
-                'last_updated': bundle_resource['meta']['lastUpdated']})
+                'last_updated': bundle_resource['meta']['lastUpdated']}
+
+            # attempt to pick up related patient
+            patient_resource = route_fhir(
+                relative_path=f"Patient/?identifier=https://cirg.washington.edu/process-message|{id}"
+            )
+
+            if True or patient_resource:
+                patient = patient_resource.json
+                for entry in patient.get('entry', []):
+                    if 'resource' not in entry:
+                        continue
+                    row['sex'] = entry['resource'].get('gender', '')
+                    try:
+                        row['name'] = ','.join((
+                            entry['resource'].get('name', [{}])[0].get('family', ''),
+                            entry['resource'].get('name', [{}])[0].get('given', [])[0]))
+                    except:
+                        pass
+                    results.append(row)
         return results
 
-    return render_template("reports.html", bundles=bundle_data())
+    return render_template(
+        "reports.html",
+        bundles=bundle_data(),
+        title="Received Case Reports")
