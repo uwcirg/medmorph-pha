9d27b7c588e5de3c4292135e5ad15ca5221481f9
Paul Bugni <pbugni@u.washington.edu>
2021-05-19 00:42:27 -0700

    very early implementation of simple `reports` table.

--- medmorph_pha/api/views.py
+++ medmorph_pha/api/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
 
 base_blueprint = Blueprint('base', __name__)
 
@@ -13,3 +13,26 @@ def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
 
     return response
+
+
+@base_blueprint.route('/reports')
+def reports():
+    """Build up table of received reports from queried FHIR data"""
+    from .fhir import route_fhir
+
+    def bundle_data():
+        """Obtain bundles and metadata such as lastUpdated for each"""
+        results = []
+        all_bundles = route_fhir(relative_path='Bundle').json
+        for bundle in all_bundles['entry']:
+            # Don't know join syntax, loop over and request metadata
+            id = bundle["resource"]["id"]
+            relative_path = f'Bundle/{id}'
+            bundle_resource = route_fhir(relative_path=relative_path).json
+            results.append({
+                'id': id,
+                'url': url_for('fhir.route_fhir', relative_path=relative_path),
+                'last_updated': bundle_resource['meta']['lastUpdated']})
+        return results
+
+    return render_template("reports.html", bundles=bundle_data())
