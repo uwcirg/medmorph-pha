15a107c11d4aba5fdc4865ce97a51d8aafed69a6
Ivan Cvitkovic <ivanc@uw.edu>
2022-03-11 11:12:17 -0800

    Allow file passed via arg

--- scripts/de-identify.sh
+++ scripts/de-identify.sh
@@ -6,18 +6,21 @@ cmdname="$(basename "$0")"
 script_path="$(cd "$(dirname "$0")" && pwd)"
 repo_path="$(readlink -f "${script_path}/..")"
 
+# default Bundle acted on, if filename not given
+default_file="${repo_path}/test-data/Bundle_cancer-report_connectathon-2022-01.json"
+
 usage() {
    cat << USAGE >&2
 Usage:
-   $cmdname [-h] [--help] fhir-server-url
+   $cmdname [-h] [--help] [file-with-phi]
    -h
    --help
           Show this help message
 
-   fhir-server-url
-          FHIR server base URL
+   file-with-phi
+          File with PHI to remove
 
-    Remove PHI
+    Remove PHI from given file, or default of $default_file
 USAGE
    exit 1
 }
@@ -26,22 +29,23 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     usage
 fi
 
+# Use given filename, or default if none given
+phi_file=${1:-$default_file}
 
 # Connectathon Patient details
-cancer_bundle_path="${repo_path}/test-data/Bundle_cancer-report_connectathon-2022-01.json"
 FNAME=MEREDITH
 LNAME=SHEPHERD
 PTID=12746018
 
 # replace first name with a fixed Study ID
-sed -i "s|$FNAME|016-002001|g" "$cancer_bundle_path"
+sed -i "s|$FNAME|016-002001|g" "$phi_file"
 
 # replace last name with phrase "Study ID"
-sed -i "s|$LNAME|Study ID|g" "$cancer_bundle_path"
+sed -i "s|$LNAME|Study ID|g" "$phi_file"
 
 # replace patient ID with "0"
 # NB replacing patient ID with the phrase "**redacted**" causes errors
-sed -i "s|$PTID|0|g" "$cancer_bundle_path"
+sed -i "s|$PTID|0|g" "$phi_file"
 
-echo Bundle file scrubbed of visible PHI:
-echo $cancer_bundle_path
+echo File scrubbed of visible PHI:
+echo $phi_file
